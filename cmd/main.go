62450034a402e963e271d0e39b6a4ffbc153d2fd
sushistack <kjm71712@naver.com>
2024-08-25 15:13:45 +0900

    Logger globalization

---
+++ cmd/main.go
@@ -0,0 +1,10 @@
+package cmd
+
+import (
+	"github.com/sushistack/link.stack/utils"
+)
+
+func main() {
+	utils.InitLogger()
+
+}
--- utils/Logger.go
+++ utils/Logger.go
@@ -6,23 +6,24 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func CustomLogger() *logrus.Logger {
-	logger := logrus.New()
+// Logger Global
+var Logger *logrus.Logger
+
+func InitLogger() {
+	Logger = logrus.New()
 
 	// 파일 핸들러 생성 (선택 사항)
 	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
-		logger.Fatal("Error opening log file: ", err)
+		Logger.Fatal("Error opening log file: ", err)
 	}
 
 	// 로그 포맷 설정
-	logger.SetFormatter(&logrus.JSONFormatter{})
+	Logger.SetFormatter(&logrus.JSONFormatter{})
 
 	// 로그 레벨 설정
-	logger.SetLevel(logrus.InfoLevel)
+	Logger.SetLevel(logrus.InfoLevel)
 
 	// 출력 설정 (파일 또는 표준 출력)
-	logger.SetOutput(file)
-
-	return logger
+	Logger.SetOutput(file)
 }
